0368d62c600bba4812ffddec8be84e33342a71a7
JarvanShi <39211618+JarvanShi@users.noreply.github.com>
2019-06-08 10:13:07 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import builtwith
-import whois
-import pprint
-
-pprint.pprint(builtwith.builtwith("http://dcloud.pingan.com"))
-pprint.pprint("******分割线******")
-pprint.pprint(whois.whois("http://dcloud.pingan.com"))
