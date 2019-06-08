42378aecdafa6526b009a9781febec4d1e8e2394
JarvanShi <Shidw94@163.com>
2019-06-08 09:33:44 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import builtwith
+import whois
+import pprint
+
+pprint.pprint(builtwith.builtwith("http://dcloud.pingan.com"))
+pprint.pprint("******分割线******")
+pprint.pprint(whois.whois("http://dcloud.pingan.com"))
